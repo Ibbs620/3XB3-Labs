6f39fe3bf6652e316a91486f432c2c1fbdd478ec
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-09-19 11:47:20 -0400

    Added insertion sort testing

--- insertion_sort_test.py
+++ insertion_sort_test.py
@@ -1,2 +1,15 @@
-import bad_sorts
+import Lab1.bad_sorts as bs
+
+runtimes = {x : [] for x in bs.list_lengths}
+
+for ll in bs.list_lengths:
+    for i in range(bs.num_runs):
+        L = bs.create_random_list(ll, bs.max_value)
+        time_taken = bs.measure_runtime(bs.insertion_sort, L)
+        runtimes[ll].append(round(time_taken, 8))
+
+for ll, results in runtimes.items():
+    print("List length: ", ll)
+    print("Runtimes: ", "\t".join(results))
+    print("---------------------------------------------")
 
