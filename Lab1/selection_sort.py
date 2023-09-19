20fd275abdc29378959d03edbce881bcc22f2903
Awurama Nyarko <nyarka1@mcmaster.ca>
2023-09-19 14:33:31 -0400

    finalizing selection_sort

--- Lab1/selection_sort.py
+++ Lab1/selection_sort.py
@@ -50,7 +50,7 @@ def measure_runtime(sorting_algorithm, L):
     end_time = time.time()
     return end_time - start_time
 
-list_lengths = [10, 50, 100, 200, 500, 1000]
+list_lengths = [100, 500, 1000, 2000, 5000, 10000]
 num_runs = 10
 max_value = 50000
 results = {algo.__name__: [] for algo in [ selection_sort]}
