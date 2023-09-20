692e8aa0cb8f3aa5471fdf4db17f64faba76c7bf
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-09-20 13:30:04 -0400

    Finished experiment 2

--- Lab1/insertion_sort_test.py
+++ Lab1/insertion_sort_test.py
@@ -1,14 +1,40 @@
 import bad_sorts as bs
 
-runtimes = {x : [] for x in bs.list_lengths}
+runtimes_old = {x : [] for x in bs.list_lengths}
+runtimes_new = {x : [] for x in bs.list_lengths}
+
+def insertion_sort_new(L):
+    for i in range(1, len(L)):
+        insert_new(L, i)
+
+
+def insert_new(L, i):
+    num = L[i]
+    while i > 0:
+        if L[i - 1] > num:
+            L[i] = L[i - 1]
+            i -= 1
+        else:
+            break
+    L[i] = num
 
 for ll in bs.list_lengths:
     for i in range(bs.num_runs):
-        L = bs.create_random_list(ll, bs.max_value)
-        time_taken = bs.measure_runtime(bs.insertion_sort, L)
-        runtimes[ll].append(round(time_taken, 6))
+        L1 = bs.create_random_list(ll, bs.max_value)
+        L2 = L1.copy()
+        #time_taken_old = bs.measure_runtime(bs.insertion_sort, L1)
+        time_taken_new = bs.measure_runtime(insertion_sort_new, L2)
+        #runtimes_old[ll].append(round(time_taken_old, 6))
+        runtimes_new[ll].append(round(time_taken_new, 6))
 
-for ll, results in runtimes.items():
+# print("**********OLD INSERTION SORT***********")
+# for ll, results in runtimes_old.items():
+#     print("List length: ", ll)
+#     print("Runtimes: ", *results)
+#     print("---------------------------------------------")
+
+print("**********NEW INSERTION SORT***********")
+for ll, results in runtimes_new.items():
     print("List length: ", ll)
     print("Runtimes: ", *results)
     print("---------------------------------------------")
