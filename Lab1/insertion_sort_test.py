c0cc8fd8be9b0b01ce9629017932987a2a66510c
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-09-19 22:55:47 -0400

    updated tests

--- Lab1/insertion_sort_test.py
+++ Lab1/insertion_sort_test.py
@@ -6,10 +6,10 @@ for ll in bs.list_lengths:
     for i in range(bs.num_runs):
         L = bs.create_random_list(ll, bs.max_value)
         time_taken = bs.measure_runtime(bs.insertion_sort, L)
-        runtimes[ll].append(time_taken)
+        runtimes[ll].append(round(time_taken, 6))
 
 for ll, results in runtimes.items():
     print("List length: ", ll)
-    print("Runtimes: ", '\t'.join(map(str, results)))
+    print("Runtimes: ", *results)
     print("---------------------------------------------")
 
