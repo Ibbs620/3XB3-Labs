81b5360694cba7e898b1e217c8c1edf9f2ddfae7
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-09-21 16:56:59 -0400

    Removed commenting

--- Lab1/insertion_sort_test.py
+++ Lab1/insertion_sort_test.py
@@ -20,14 +20,14 @@ def insert_new(L, i):
             break
     L[i] = num
 
-# for ll in bs.list_lengths:
-#     for i in range(bs.num_runs):
-#         L1 = bs.create_random_list(ll, bs.max_value)
-#         L2 = L1.copy()
-#         #time_taken_old = bs.measure_runtime(bs.insertion_sort, L1)
-#         time_taken_new = bs.measure_runtime(insertion_sort_new, L2)
-#         #runtimes_old[ll].append(round(time_taken_old, 6))
-#         runtimes_new[ll].append(round(time_taken_new, 6))
+for ll in bs.list_lengths:
+    for i in range(bs.num_runs):
+        L1 = bs.create_random_list(ll, bs.max_value)
+        L2 = L1.copy()
+        time_taken_old = bs.measure_runtime(bs.insertion_sort, L1)
+        time_taken_new = bs.measure_runtime(insertion_sort_new, L2)
+        runtimes_old[ll].append(round(time_taken_old, 6))
+        runtimes_new[ll].append(round(time_taken_new, 6))
 
 for s in swaps:
     for i in range(bs.num_runs):
@@ -38,19 +38,19 @@ for s in swaps:
 
 
 
-# print("**********OLD INSERTION SORT***********")
-# for ll, results in runtimes_old.items():
-#     print("List length: ", ll)
-#     print("Runtimes: ", *results)
-#     print("---------------------------------------------")
-
-# print("**********NEW INSERTION SORT***********")
-# for ll, results in runtimes_new.items():
-#     print("List length: ", ll)
-#     print("Runtimes: ", *results)
-#     print("---------------------------------------------")
+print("**********OLD INSERTION SORT***********")
+for ll, results in runtimes_old.items():
+    print("List length: ", ll)
+    print("Runtimes: ", *results)
+    print("---------------------------------------------")
 
 print("**********NEW INSERTION SORT***********")
+for ll, results in runtimes_new.items():
+    print("List length: ", ll)
+    print("Runtimes: ", *results)
+    print("---------------------------------------------")
+
+print("**********NEW INSERTION SORT (SWAP TEST)***********")
 for s, results in runtimes_swap.items():
     print("Swaps: ", s)
     print("Runtimes: ", *results)
