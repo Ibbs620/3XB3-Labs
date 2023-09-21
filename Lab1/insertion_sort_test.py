bbae41f3011b679b7f4972cc197e00192185cc6e
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-09-21 16:55:57 -0400

    Completed exp 3

--- Lab1/insertion_sort_test.py
+++ Lab1/insertion_sort_test.py
@@ -1,7 +1,9 @@
 import bad_sorts as bs
 
+swaps = [10, 50, 100, 500, 1000, 5000]
 runtimes_old = {x : [] for x in bs.list_lengths}
 runtimes_new = {x : [] for x in bs.list_lengths}
+runtimes_swap = {x : [] for x in swaps}
 
 def insertion_sort_new(L):
     for i in range(1, len(L)):
@@ -18,14 +20,23 @@ def insert_new(L, i):
             break
     L[i] = num
 
-for ll in bs.list_lengths:
+# for ll in bs.list_lengths:
+#     for i in range(bs.num_runs):
+#         L1 = bs.create_random_list(ll, bs.max_value)
+#         L2 = L1.copy()
+#         #time_taken_old = bs.measure_runtime(bs.insertion_sort, L1)
+#         time_taken_new = bs.measure_runtime(insertion_sort_new, L2)
+#         #runtimes_old[ll].append(round(time_taken_old, 6))
+#         runtimes_new[ll].append(round(time_taken_new, 6))
+
+for s in swaps:
     for i in range(bs.num_runs):
-        L1 = bs.create_random_list(ll, bs.max_value)
+        L1 = bs.create_near_sorted_list(1000, bs.max_value, s)
         L2 = L1.copy()
-        #time_taken_old = bs.measure_runtime(bs.insertion_sort, L1)
-        time_taken_new = bs.measure_runtime(insertion_sort_new, L2)
-        #runtimes_old[ll].append(round(time_taken_old, 6))
-        runtimes_new[ll].append(round(time_taken_new, 6))
+        time_taken = bs.measure_runtime(insertion_sort_new, L2)
+        runtimes_swap[s].append(round(time_taken, 6))
+
+
 
 # print("**********OLD INSERTION SORT***********")
 # for ll, results in runtimes_old.items():
@@ -33,9 +44,15 @@ for ll in bs.list_lengths:
 #     print("Runtimes: ", *results)
 #     print("---------------------------------------------")
 
+# print("**********NEW INSERTION SORT***********")
+# for ll, results in runtimes_new.items():
+#     print("List length: ", ll)
+#     print("Runtimes: ", *results)
+#     print("---------------------------------------------")
+
 print("**********NEW INSERTION SORT***********")
-for ll, results in runtimes_new.items():
-    print("List length: ", ll)
+for s, results in runtimes_swap.items():
+    print("Swaps: ", s)
     print("Runtimes: ", *results)
     print("---------------------------------------------")
 
