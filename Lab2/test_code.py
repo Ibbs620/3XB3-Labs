d844cde0738241e2626f3cb2264a07704752a9f2
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-10-05 13:30:40 -0400

    Added more test code

--- Lab2/test_code.py
+++ Lab2/test_code.py
@@ -9,6 +9,9 @@ G.add_edge(1,3)
 G.add_edge(3,4)
 G.add_edge(4,5)
 
-correct_pred = {2 : 1, 3 : 1, 4 : 2, 5 : 3, 6 : 4}
-returned_pred = BFS3(G, 1)
-assert correct_pred == returned_pred
+correct_bfs_pred = {2 : 1, 3 : 1, 4 : 2, 5 : 3, 6 : 4}
+bfs_pred = BFS3(G, 1)
+dfs_pred = DFS3(G, 1)
+assert correct_bfs_pred == bfs_pred
+print(dfs_pred)
+print(bfs_pred)
