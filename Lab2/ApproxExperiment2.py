3b9c4128cb17fe317538a1d25d8609a282a1db58
Rayyan Suhail <suhailr@mcmaster.ca>
2023-10-26 18:24:01 -0400

    Updated code

--- Lab2/ApproxExperiment2.py
+++ Lab2/ApproxExperiment2.py
@@ -1,8 +1,7 @@
 from graph import *
 
-number_of_nodes = [5,10,15,20,25,30]
-
+number_of_edges = [10,20,30,40,50,60,70,80,90,100]
 
 for _ in range(1000):
-    for node in number_of_nodes:
+    for node in number_of_edges:
         print(approx3(create_random_graph(node, int(node * 1.5))))
--- Lab2/test_code.py
+++ Lab2/test_code.py
@@ -51,3 +51,44 @@ DG2.add_edge(2,0)
 DG2.add_edge(0,1)
 print(DG2.adj)
 assert has_cycle(DG2)
+
+
+##############################################################################################################################
+# has_cycle testing code
+#############################################################################################################################
+
+def test_case_1():
+    G = Graph(4)
+    G.add_edge(0, 1)
+    G.add_edge(1, 2)
+    G.add_edge(2, 3)
+    G.add_edge(3, 0)
+    
+    assert has_cycle(G) == True, "Test Case 1 Failed"
+
+# A tree structure (acyclic graph)
+def test_case_2():
+    G = Graph(4)
+    G.add_edge(0, 1)
+    G.add_edge(1, 2)
+    G.add_edge(1, 3)
+    
+    assert has_cycle(G) == False, "Test Case 2 Failed"
+
+# A disconnected graph with one component having a cycle
+def test_case_3():
+    G = Graph(5)
+    G.add_edge(0, 1)
+    G.add_edge(1, 2)
+    G.add_edge(2, 0)
+    # The graph has another component with nodes 3 and 4 which are disconnected from the main graph and from each other
+    
+    assert has_cycle(G) == True, "Test Case 3 Failed"
+
+def run_tests():
+    test_case_1()
+    test_case_2()
+    test_case_3()
+    print("All Test Cases Passed!")
+
+run_tests()
