e9697201599cd493c22c40b6475f2a1402be4acd
Awurama Nyarko <nyarka1@mcmaster.ca>
2023-11-07 08:17:32 -0500

    graph lab edit

--- Lab2/Experiment2.py
+++ Lab2/Experiment2.py
@@ -1,25 +1,31 @@
 from graph import *
+import matplotlib.pyplot as plt
 
-def experiment_2(num_nodes, max_edges, m=1000):
-    results_2 = {}
-    
-    for num_edges in range(num_nodes, max_edges + 1):
+def experiment_2(num_nodes, edge_counts, num_graphs_per_edge):
+    connection_probabilities = []
+
+    for edges in edge_counts:
         connected_count = 0
-        
-        for _ in range(m):
-            G = create_random_graph(num_nodes, num_edges)
+        for _ in range(num_graphs_per_edge):
+            G = create_random_graph(num_nodes, edges)
             if is_connected(G):
                 connected_count += 1
+        probability = round(connected_count / num_graphs_per_edge, 10)
+        connection_probabilities.append(probability)
+        print("Number of edges: ", edges, "Probability of being connected: ", probability)
 
-        proportion_of_edges = num_edges / (num_nodes * (num_nodes - 1))
-        connected_probability = connected_count / m
-        results_2[proportion_of_edges] = connected_probability
+    plt.plot(edge_counts, connection_probabilities, marker='.')
+    plt.xlabel('Number of Edges')
+    plt.ylabel('Probability of Being Connected')
+    plt.title('Edge Count vs Connectivity Probability')
+    plt.grid(True)
+    plt.show()
 
-    return results_2
-
-# Experiment 2 testing
+# Example usage:
+# Define your edge_counts and num_nodes
+edge_counts = list(range(0, 180, 5))
 num_nodes = 100
-max_edges = num_nodes * (num_nodes - 1) // 2
-results_2 = experiment_2(num_nodes, max_edges)
+num_graphs_per_edge = 500
 
-print(results_2)
+# Run the experiment
+experiment_2(num_nodes, edge_counts, num_graphs_per_edge)
