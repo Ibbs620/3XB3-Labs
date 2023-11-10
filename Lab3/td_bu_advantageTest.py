4640695c6ceb08a5201e73fa602a1f5e73a7135f
Awurama Nyarko <nyarka1@mcmaster.ca>
2023-11-09 20:37:22 -0500

    testing fix

--- Lab3/td_bu_advantageTest.py
+++ Lab3/td_bu_advantageTest.py
@@ -3,28 +3,30 @@ import random
 import matplotlib.pyplot as plt
 from knapsack import ks_top_down, ks_bottom_up
 
+num_trials = 1
 
 def generate_item_set(num_items, max_weight, max_value, scenario):
     if scenario == "bottom_up_advantage":
-        # Create an item set with low weights and high values
+        # item set with low weights and high values
         items = [(random.randint(1, 5), random.randint(50, 100)) for _ in range(num_items)]
     else:
-        # Create an item set with varying weights and values
+        # item set with varying weights and values
         items = [(random.randint(1, max_weight), random.randint(1, max_value)) for _ in range(num_items)]
     return items
 
+def measure_average_runtime(knapsack_func, items, capacity, num_trials):
+    total_runtime = 0
+    for _ in range(num_trials):
+        start_time = time.time()
+        knapsack_func(items, capacity)
+        end_time = time.time()
+        total_runtime += (end_time - start_time)
+    return total_runtime / num_trials
 
-def measure_runtime(knapsack_func, items, capacity):
-    start_time = time.time()
-    knapsack_func(items, capacity)
-    end_time = time.time()
-    return end_time - start_time
+item_set_sizes = [100, 200, 300, 400, 500]  
 
-
-item_set_sizes = [10, 20, 30, 40, 50]
 max_weight = 50
 max_value = 100
-
 
 runtimes_td_bottom_up_adv = []
 runtimes_bu_bottom_up_adv = []
@@ -36,15 +38,17 @@ for num_items in item_set_sizes:
     items_top_down_adv = generate_item_set(num_items, max_weight, max_value, "top_down_advantage")
     capacity = num_items * max_weight
 
-    runtime_td_bottom_up_adv = measure_runtime(ks_top_down, items_bottom_up_adv, capacity)
-    runtime_bu_bottom_up_adv = measure_runtime(ks_bottom_up, items_bottom_up_adv, capacity)
-    runtime_td_top_down_adv = measure_runtime(ks_top_down, items_top_down_adv, capacity)
-    runtime_bu_top_down_adv = measure_runtime(ks_bottom_up, items_top_down_adv, capacity)
+    runtime_td_bottom_up_adv = measure_average_runtime(ks_top_down, items_bottom_up_adv, capacity, num_trials)
+    runtime_bu_bottom_up_adv = measure_average_runtime(ks_bottom_up, items_bottom_up_adv, capacity, num_trials)
+    runtime_td_top_down_adv = measure_average_runtime(ks_top_down, items_top_down_adv, capacity, num_trials)
+    runtime_bu_top_down_adv = measure_average_runtime(ks_bottom_up, items_top_down_adv, capacity, num_trials)
 
     runtimes_td_bottom_up_adv.append(runtime_td_bottom_up_adv)
     runtimes_bu_bottom_up_adv.append(runtime_bu_bottom_up_adv)
     runtimes_td_top_down_adv.append(runtime_td_top_down_adv)
     runtimes_bu_top_down_adv.append(runtime_bu_top_down_adv)
+
+
 
 plt.figure(figsize=(12, 6))
 
