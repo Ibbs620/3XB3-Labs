ce2f570e3c2d20d5811ffd4f8bc6762f39896c08
Rayyan Suhail <suhailr@mcmaster.ca>
2023-11-22 16:17:02 -0500

    Added exp_1.2

---
+++ FinalProject/exp_1.2.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import time
+import random
+from final_project_part1 import *
+
+# Experiment setup
+constant_graph_size = 100  # Fixed graph size
+k_values = [1, 2, 5, 10, 20]  # Different values for k
+dijkstra_times = []
+bellman_ford_times = []
+
+# Create a graph with a constant number of nodes and random edges
+G = DirectedWeightedGraph()
+for node in range(constant_graph_size):
+    G.add_node(node)
+for node1 in range(constant_graph_size):
+    for node2 in range(constant_graph_size):
+        if node1 != node2 and random.random() < 0.3:  # 30% chance to create an edge
+            G.add_edge(node1, node2, random.randint(1, 10))
+
+# Run experiments for different values of k
+for k in k_values:
+    # Measure time for dijkstra_approx
+    start_time = time.time()
+    dijkstra_approx(G, 0, k)
+    dijkstra_times.append(time.time() - start_time)
+
+    # Measure time for bellman_ford_approx
+    start_time = time.time()
+    bellman_ford_approx(G, 0, k)
+    bellman_ford_times.append(time.time() - start_time)
+
+# Plotting
+plt.figure(figsize=(10, 6))
+plt.plot(k_values, dijkstra_times, label='Dijkstra Approx', marker='o')
+plt.plot(k_values, bellman_ford_times, label='Bellman-Ford Approx', marker='x')
+plt.xlabel('Value of k (Relaxation Limit)')
+plt.ylabel('Execution Time (Seconds)')
+plt.title('Performance of Dijkstra and Bellman-Ford Approximations Varying k (Graph Size = {})'.format(constant_graph_size))
+plt.legend()
+plt.grid(True)
+plt.show()
