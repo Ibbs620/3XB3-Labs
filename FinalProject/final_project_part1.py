c3f8e415ee56e1e84f72434c577b2a60bec8063b
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-11-24 21:06:27 -0500

    Completed A* implemetation

--- FinalProject/final_project_part1.py
+++ FinalProject/final_project_part1.py
@@ -170,3 +170,36 @@ def bellman_ford_approx(G, source, k):
                         break
 
     return dist
+
+########################################################################################################################################################################
+# PART 2 IMPLEMENTATIONS
+########################################################################################################################################################################
+def a_star(G, s, d, h):
+    pred = {}
+    dist = {} 
+    Q = min_heap2.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        Q.insert(min_heap2.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(s, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        if current_node == d:
+            break
+        dist[current_node] = current_element.key - h[current_node]
+        for neighbour in G.adj[current_node]:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour) + h[neighbour])
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+
+    path = [next := pred[d], d]
+    while next != s:
+        next = pred[next]
+        path.insert(0, next)
+
+    return pred, path
