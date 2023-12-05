d5040c6f2c8b70ab3bffe9d4124d1276e68d6350
Rayyan Suhail <suhailr@mcmaster.ca>
2023-12-05 11:12:47 -0500

    Refactored part 4 code

--- FinalProject/broken_station_problem.py
+++ FinalProject/broken_station_problem.py
@@ -1,66 +1,43 @@
 def isFeasible(mid, arr, n, k):
-    pos = arr[0]
-    elements = 1
+    pos, elements = arr[0], 1
     for i in range(1, n):
-        if (arr[i] - pos >= mid):
-            pos = arr[i]
-            elements += 1
-            if (elements == k):
+        if arr[i] - pos >= mid:
+            pos, elements = arr[i], elements + 1
+            if elements == k:
                 return True
     return False
 
-def bsp_value(L, m):
-    n = len(L)
-    L.sort()  
-    k = n - m
-    left = 1
-    right = L[n - 1]
-    res = -1
-
-    while (left < right):
-        mid = (left + right) // 2  
-        if (isFeasible(mid, L, n, k)):
-            res = max(res, mid)
-            left = mid + 1
-        else:
-            right = mid
-
-    return res
-
-def bsp_solution(L, m):
-    n = len(L)
-    L.sort()
-    k = n - m
-    left = 1
-    right = L[n - 1]
-    res = -1
+def findBestMid(L, n, k):
+    left, right = 1, L[-1]
     best_mid = 0
 
-    while (left < right):
+    while left < right:
         mid = (left + right) // 2
-
-        if (isFeasible(mid, L, n, k)):
-            if mid > best_mid:
-                best_mid = mid
-                res = mid
+        if isFeasible(mid, L, n, k):
+            best_mid = max(best_mid, mid)
             left = mid + 1
         else:
             right = mid
 
-    result = []
-    last_added = L[0]
-    result.append(last_added)
-    elements = 1
+    return best_mid
 
-    for i in range(1, n):
-        if (elements < k) and (L[i] - last_added >= best_mid):
+def bsp_value(L, m):
+    L.sort()
+    return findBestMid(L, len(L), len(L) - m)
+
+def bsp_solution(L, m):
+    L.sort()
+    best_mid = findBestMid(L, len(L), len(L) - m)
+    result, last_added = [L[0]], L[0]
+
+    for i in range(1, len(L)):
+        if len(result) < len(L) - m and L[i] - last_added >= best_mid:
             result.append(L[i])
             last_added = L[i]
-            elements += 1
 
     return result
 
-
+# Test the functions
 val = bsp_value([2, 4, 6, 7, 10, 14], 2)
 print(val)
 assert val == 4
