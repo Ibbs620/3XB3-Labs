2822f853dc0620c98223cf2760470efa4bb319ba
Rayyan Suhail <suhailr@mcmaster.ca>
2023-12-05 11:05:44 -0500

    Updated solution for part 4

--- FinalProject/broken_station_problem.py
+++ FinalProject/broken_station_problem.py
@@ -1,15 +1,70 @@
+def isFeasible(mid, arr, n, k):
+    pos = arr[0]
+    elements = 1
+    for i in range(1, n):
+        if (arr[i] - pos >= mid):
+            pos = arr[i]
+            elements += 1
+            if (elements == k):
+                return True
+    return False
+
 def bsp_value(L, m):
-    if m >= len(L):
-        return 0
+    n = len(L)
+    L.sort()  
+    k = n - m
+    left = 1
+    right = L[n - 1]
+    res = -1
 
-    max_diff = 0
+    while (left < right):
+        mid = (left + right) // 2  
+        if (isFeasible(mid, L, n, k)):
+            res = max(res, mid)
+            left = mid + 1
+        else:
+            right = mid
 
-    for i in range(len(L) - m):
-        diff = L[m + i] - L[i]
+    return res
 
-        if diff > max_diff:
-            max_diff = diff
+def bsp_solution(L, m):
+    n = len(L)
+    L.sort()
+    k = n - m
+    left = 1
+    right = L[n - 1]
+    res = -1
+    best_mid = 0
 
-    return max_diff
+    while (left < right):
+        mid = (left + right) // 2
 
-print(bsp_value([2, 4, 6, 7, 10, 14], 2))
+        if (isFeasible(mid, L, n, k)):
+            if mid > best_mid:
+                best_mid = mid
+                res = mid
+            left = mid + 1
+        else:
+            right = mid
+
+    result = []
+    last_added = L[0]
+    result.append(last_added)
+    elements = 1
+
+    for i in range(1, n):
+        if (elements < k) and (L[i] - last_added >= best_mid):
+            result.append(L[i])
+            last_added = L[i]
+            elements += 1
+
+    return result
+
+
+val = bsp_value([2, 4, 6, 7, 10, 14], 2)
+print(val)
+assert val == 4
+
+sol = bsp_solution([2, 4, 6, 7, 10, 14], 2)
+print(sol)
+assert sol == [2, 6, 10, 14]
