6d94561b452cdddba627c6000d6bf9001bb1904f
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-12-07 22:51:58 -0500

    Added comments

--- FinalProject/broken_station_problem.py
+++ FinalProject/broken_station_problem.py
@@ -7,11 +7,15 @@ def solve_bsp(stations, m):
     for i in range(1, n):
         DP[0][i] = min(dist[:i])
     
+    # Fill out DP table
     for i in range(1, m+1):
         for j in range(i + 1, n):
             DP[i][j] = 0
             for k in range(i + 1):
                 DP[i][j] = max(DP[i][j], min(abs(stations[j-k-1] - stations[j]), DP[i - k][j - k - 1]))
+
+            # DP Formula:
+            # DP[i][j] = max( min( |station[j - k - 1] - station[j]| ), DP[i - k][j - k - 1] ) for 0 <= k <= i
     
     return DP
 
@@ -22,22 +26,26 @@ def bsp_value(stations, m):
 def bsp_solution(stations, m):
     DP = solve_bsp(stations, m)
     solution = stations.copy()
+
+    # Start at bottom right of table
     i = m
     j = len(stations)-1
-    while(j >= 0):
+
+    while(j >= 0): 
         if i <= 0:
             j -= 1
             i = m
             continue
+
         for k in range(1, i + 1):
-            s = min(abs(stations[j-k-1] - stations[j]), DP[i - k][j - k - 1])
-            if DP[i][j] == s:
-                i -= k
-                j -= 1
-                m -= 1
-                solution.remove(stations[j])
+            s = min(abs(stations[j-k-1] - stations[j]), DP[i - k][j - k - 1]) 
+            if DP[i][j] == s: # Check if the optimal k for station j is not 0
+                j -= k
+                m -= k
+                i = m
+                solution.remove(stations[j]) # If k != 0, station j - 1 is removed
                 continue
-        i -= 1
+            
     return solution
 
 # Test
