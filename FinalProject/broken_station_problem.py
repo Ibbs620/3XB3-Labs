f3fe98c43fb24c9046632d5a0b2a4f9548898686
Ibrahim Quraishi <quraishi@mcmaster.ca>
2023-12-07 22:44:02 -0500

    Completed broken station problem

--- FinalProject/broken_station_problem.py
+++ FinalProject/broken_station_problem.py
@@ -1,47 +1,51 @@
-def isFeasible(mid, arr, n, k):
-    pos, elements = arr[0], 1
+def solve_bsp(stations, m):
+    n = len(stations)
+    DP = [[float('inf') for _ in range(n)] for _ in range(m+1)]
+
+    # Base case m = 0
+    dist = [abs(stations[i] - stations[i-1]) for i in range(1, n)]
     for i in range(1, n):
-        if arr[i] - pos >= mid:
-            pos, elements = arr[i], elements + 1
-            if elements == k:
-                return True
-    return False
+        DP[0][i] = min(dist[:i])
+    
+    for i in range(1, m+1):
+        for j in range(i + 1, n):
+            DP[i][j] = 0
+            for k in range(i + 1):
+                DP[i][j] = max(DP[i][j], min(abs(stations[j-k-1] - stations[j]), DP[i - k][j - k - 1]))
+    
+    return DP
 
-def findBestMid(L, n, k):
-    left, right = 1, L[-1]
-    best_mid = 0
+def bsp_value(stations, m):
+    DP = solve_bsp(stations, m)
+    return DP[m][len(stations)-1]
 
-    while left < right:
-        mid = (left + right) // 2
-        if isFeasible(mid, L, n, k):
-            best_mid = max(best_mid, mid)
-            left = mid + 1
-        else:
-            right = mid
+def bsp_solution(stations, m):
+    DP = solve_bsp(stations, m)
+    solution = stations.copy()
+    i = m
+    j = len(stations)-1
+    while(j >= 0):
+        if i <= 0:
+            j -= 1
+            i = m
+            continue
+        for k in range(1, i + 1):
+            s = min(abs(stations[j-k-1] - stations[j]), DP[i - k][j - k - 1])
+            if DP[i][j] == s:
+                i -= k
+                j -= 1
+                m -= 1
+                solution.remove(stations[j])
+                continue
+        i -= 1
+    return solution
 
-    return best_mid
+# Test
+stations = [2, 4, 6, 7, 10, 14]
+m = 2
 
-def bsp_value(L, m):
-    L.sort()
-    return findBestMid(L, len(L), len(L) - m)
-
-def bsp_solution(L, m):
-    L.sort()
-    best_mid = findBestMid(L, len(L), len(L) - m)
-    result, last_added = [L[0]], L[0]
-
-    for i in range(1, len(L)):
-        if len(result) < len(L) - m and L[i] - last_added >= best_mid:
-            result.append(L[i])
-            last_added = L[i]
-
-    return result
-
-# Test the functions
-val = bsp_value([2, 4, 6, 7, 10, 14], 2)
-print(val)
-assert val == 4
-
-sol = bsp_solution([2, 4, 6, 7, 10, 14], 2)
-print(sol)
-assert sol == [2, 6, 10, 14]
+# Print table, value, and solution
+for x in solve_bsp(stations, m):
+    print('\t'.join(map(str, x)))
+print(bsp_value(stations, m))
+print(bsp_solution(stations, m))
